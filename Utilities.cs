52dcbf1e26bd257aa5ab8e6b69f46c089c123b45
ron-popov <ron.popov@hotmail.com>
2017-12-06 11:22:12 +0200

    Get bitmap from http url

--- Utilities.cs
+++ Utilities.cs
@@ -1,14 +1,24 @@
 using System;
 using System.Drawing;
+using System.Net;
+using System.IO;
 
 class Utilities
 {
-    public static string ToBase64(this Bitmap b)
+    public static string ToBase64(Bitmap b)
     {
         //bitmap to byte array
         ImageConverter converter = new ImageConverter();
         byte[] byteArray = (byte[])converter.ConvertTo(b, typeof(byte[]));
         string base64String = Convert.ToBase64String(byteArray);
         return base64String;
+    }
+
+    public static Bitmap GetBitmapFromHttp(string url)
+    {
+        WebClient wc = new WebClient();
+        Stream s = wc.OpenRead(url);
+        Bitmap bmp = new Bitmap(s);
+        return bmp;
     }
 }
