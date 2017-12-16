ca242cd953018cf5952d8260d9a3ed86eaed765a
Yotam Salmon <yotam.salmon@gmail.com>
2017-12-16 14:19:34 +0200

    Whoops. Right project is now uploaded

--- Yotam's app/InkTestWPF/Properties/Resources.Designer.cs
+++ Yotam's app/InkTestWPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace InkTest.Properties
+namespace InkTestWPF.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace InkTest.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("InkTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("InkTestWPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
